fd4b8358cba665d9eb6b81fc29a854e8eb3f6c18
rodneypinto <48834490+rodney198@users.noreply.github.com>
2026-03-27 01:43:25 +0530

    feat: APIManager Generic Class added
    Generic API Manager Class added  to make the network calls and handle nework error

---
+++ Tahudu/Manager/APIManager.swift
@@ -0,0 +1,69 @@
+//
+//  APIManager.swift
+//  Tahudu
+//
+
+import Foundation
+
+enum APIError: Error {
+    case invalidResponse
+    case httpStatusError(code: Int)
+    case decoding(Error)
+    case underlying(URLError)
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .invalidResponse:
+            return "Invalid response format"
+        case let .httpStatusError(code):
+            return "Request Failed HTTP status code: \(code)"
+        case let .decoding(error):
+            return "Failed to decode response: \(error.localizedDescription)"
+        case let .underlying(urlError):
+            return urlError.localizedDescription
+        }
+    }
+}
+
+protocol APIManagerProtocol {
+    func request<T: Decodable>(_ url: URL, as type: T.Type) async throws -> T
+}
+
+final class APIManager: APIManagerProtocol {
+    
+    private let session: URLSession
+    private let decoder: JSONDecoder
+    
+    init(session: URLSession, decoder: JSONDecoder) {
+        self.session = session
+        self.decoder = decoder
+    }
+    
+    func request<T: Decodable>(_ url: URL, as type: T.Type) async throws-> T {
+        let data : Data
+        let response: URLResponse
+        do {
+            (data, response) = try await session.data(from: url)
+        } catch let urlSessionError as URLError {
+            throw APIError.underlying(urlSessionError)
+        } catch {
+            throw APIError.underlying(URLError(.cannotLoadFromNetwork))
+        }
+        
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw APIError.invalidResponse
+        }
+        
+        guard (200...299).contains(httpResponse.statusCode) else {
+            throw APIError.httpStatusError(code: httpResponse.statusCode)
+        }
+        
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            throw APIError.decoding(error)
+        }
+    }
+}
